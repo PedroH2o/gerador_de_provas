75dcd5de49d252db47690c11fac997f6c375ff2a
David Felipe <felipe30.10@outlook.com>
2023-09-28 13:42:02 -0300

    setup project with Fastify and Prisma

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+
+
+const app = Fastify()
+
+// Declare a route
+app.get('/', async function handler (request, reply) {
+  return 'hello world'
+})
+
+// Run the server!
+try {
+  await app.listen({ port: 3000 }).then(() => {
+    console.log('HTTP Server running!')
+})
+} catch (err) {
+  app.log.error(err)
+  process.exit(1)
+}
